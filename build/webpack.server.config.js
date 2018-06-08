381889992b4814bb02dcbed51bd3c851ba6bd318
Yang Ke <saint_yk@163.com>
2018-06-08 15:56:52 +0800

    Create server side entry and webpack config files

---
+++ build/webpack.server.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = {
+  target: 'node',
+  entry: {
+    app: path.join(__dirname, '../client/server-entry.js',)
+  },
+
+  output: {
+    filename: 'server-entry.js',
+    path: path.join(__dirname, '../dist'),
+    publicPath: '',
+    libraryTarget: 'commonjs2'
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      }
+    ]
+  }
+}
