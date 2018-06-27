7958e9ac20357bd8e47788157dc0569c29681e6f
Yang Ke <saint_yk@163.com>
2018-06-27 15:22:00 +0800

    Enable ES7 decorators for mobx

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -19,9 +19,9 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['es2015', 'react']
-          }
+          // options: {
+          //   presets: ['es2015', 'react']
+          // }
         }
       }
     ]
