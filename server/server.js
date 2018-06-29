7b2348fad706918ba5be96016d51a401854cd775
Yang Ke <saint_yk@163.com>
2018-06-29 09:33:04 +0800

    Fix 'body-parser' parameter 'extended' typo

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express()
 const isDev = process.env.NODE_ENV === 'development'
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extend: false }))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(session({
   maxAge: 10 * 60 * 1000, // 10 minutes
   name: 'tid',
